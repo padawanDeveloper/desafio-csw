0cbca53e1d68c165fb40858732475df75662a862
Esteban Ortiz <ematiasortiz@gmail.com>
2020-06-17 20:32:25 -0400

    changed map to librery leaflet

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
-
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-});
+import Map from './components/Map';
 
 function App() {
   const [data, setData] = useState([]);
@@ -32,26 +22,9 @@ function App() {
   if (loading) return <h1>Cargando...</h1>;
   if (error) return <h1>Error al cargar el mapa..</h1>;
 
-  const position = data[0].geometry.coordinates;
   return (
-    <div className="App">
-      <Map center={position} zoom={15} style={{ height: '100vh' }}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-        {data.map(obj => (
-          <Marker key={obj.properties.id} position={obj.geometry.coordinates}>
-            <Popup>
-              <h2>{obj.properties.name}</h2>
-              <div>
-                <strong>Latitude:</strong> {obj.geometry.coordinates[0]} |{' '}
-                <strong>Longitude:</strong> {obj.geometry.coordinates[1]}
-              </div>
-            </Popup>
-          </Marker>
-        ))}
-      </Map>
+    <div>
+      <Map data={data} />
     </div>
   );
 }
